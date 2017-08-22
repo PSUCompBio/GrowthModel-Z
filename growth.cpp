219dd510eab76be3e2dac0ea22219720f9c85e22
Reuben Kraft <reuben.kraft@gmail.com>
2017-08-22 15:20:16 -0400

    modifications

--- growth.cpp
+++ growth.cpp
@@ -5,33 +5,21 @@
 #include <vector>
 using namespace std;
 
-
-int main()
-{
-	int nodes = 100;
-	int z = 0;
-	//supppose the range of the random position is 0-100
-	//suppose record the postion at each z point
-	for (int i = 0; i <= nodes; i++)
-	{
-		int x= rand() % 100;
-		int y= rand() % 100;
-		int z=i;
-	}
-    return 0;
-}
-
 int main()
 {
 	//an array of vector
 	//supose only record 100 nodes in an axon,so the array has 100 elements
 	int nodes = 100;
-	vector <int> position[100];
+	vector <double> coordinates[nodes];
 	for (int i = 0; i < nodes; i++)
 	{
-		position[i].push_back (rand() % 100);
-		position[i].push_back (rand() % 100);
-		position[i].push_back (i);
+                double x= rand() % 100;
+                double y= rand() % 100;
+                double z=i*1.0;
+		coordinates[i].push_back (x);
+		coordinates[i].push_back (y);
+		coordinates[i].push_back (z);
+		cout <<"Coordinate of node "<<i<<":  "<<x<<", "<<y<<", "<<z<<endl; 
 	}
 	return 0;
 }
