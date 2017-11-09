248b133eeca1308479a1b9090129dd274fad9e30
Srikumar Sridhar <srikumar947@gmail.com>
2017-11-09 16:53:02 -0500

    Update growth.cpp

--- growth.cpp
+++ growth.cpp
@@ -9,9 +9,10 @@ using namespace std;
 
 // Global variables
 
-int dim = 3;          							// Number of Dimensions - (x, y, z)
-int nodes;            							// Number of Nodes
-unordered_map <int, int> connectivity;					// Connectivity Matrix
+int dim = 3;          						// Number of Dimensions - (x, y, z)
+int cells;							// Number of Cells
+int *nodes = NULL;            					// Number of Nodes
+unordered_map <int, int> connectivity;				// Connectivity Matrix
 unordered_map <int, int> check;
 
 struct Node
@@ -72,14 +73,27 @@ void printList (Node* head)
 void printConnectivity (Node* head)
 {
 	Node *l = head;
+	int addOff = 0, i = 0, counter = 0;
 
 	cout << "\n Connectivity Matrix - \n";
 
-	while (l && l->next)
+	for (i = 0; i < cells; i++)
 	{
-		cout << " " << l->value << " -> " << connectivity[l->value];
-		cout << endl;
-		l = l->next;
+		cout <<"\n Cell " << i + 1 << " - \n";
+		
+		if (i != 0)
+			addOff += nodes[i - 1];
+
+		for (counter = 1; counter < nodes[i]; counter++)
+		{
+			if (l && l->next)
+				cout << " " << l->value + addOff << " -> " << connectivity[l->value + addOff] << endl;
+
+			l = l->next;
+		}
+
+		if (l)
+			l = l->next;
 	}
 
 }
@@ -87,52 +101,74 @@ void printConnectivity (Node* head)
 void updateConnectivity (Node *head)
 {
 	Node *l = head;
-	
-	while (l)
-	{
-		if (l && l->next)
-			connectivity[l->value] = l->next->value;
+	int addOff = 0, i = 0, counter = 0;
 
-		l = l->next;
+	for (i = 0; i < cells; i++)
+	{
+		if (i != 0)
+			addOff += nodes[i - 1];
+
+		for (counter = 1; counter < nodes[i]; counter++)
+		{
+			if (l && l->next)
+				connectivity[l->value + addOff] = l->next->value + addOff;
+
+			l = l->next;
+		}
+
+		if (l)
+			l = l->next;
 	}
 }
 
 Node* createNodes (Node* head)
 {
 	Node *l = NULL;
-	int i = 0, j = 0, flag = 1, temp[4];
+	int x = 0, k = 0, i = 0, j = 0, flag = 1, temp[4];
 
-	cout << "\n Enter the number of nodes: ";
-	cin >> nodes;
+	cout << "\n Enter the number of cells: ";
+	cin >> cells;
 
-	for (i = 0; i < nodes; i++)
+	nodes = new int[cells];
+	
+	for (i = 0; i < cells; i++)
 	{
-		j = 0;
-
-		temp[0] = randomGen(1, nodes);
-		check[temp[0]] = 1;
-
-		for (j = 1; j <= 3; j++)
-		{
-			temp[j] = randomGen(-10 - nodes, nodes + 10);
-		}
-
-		Node *pointer = new Node(temp[0], temp[1], temp[2], temp[3]);
-		
-		if (flag == 1)
-		{
-			head = pointer;
-			l = pointer;
-			flag = 0;
-		}
-
-		else
-		{
-			l->next = pointer;
-			l = pointer;
-		}
+		cout << "\n Enter the number of nodes for cell " << i + 1 << ": ";
+		cin >> nodes[i];
 	}
 
+	for (k = 0; k < cells; k++)
+	{
+		x = nodes[k];
+		check.clear();
+		
+		for (i = 0; i < x; i++)
+		{
+			j = 0;
+
+			temp[0] = randomGen(1, x);
+			check[temp[0]] = 1;
+
+			for (j = 1; j <= 3; j++)
+				temp[j] = randomGen(-1000 - x, x + 1000);
+
+			Node *pointer = new Node(temp[0], temp[1], temp[2], temp[3]);
+			
+			if (flag == 1)
+			{
+				head = pointer;
+				l = pointer;
+				flag = 0;
+			}
+
+			else
+			{
+				l->next = pointer;
+				l = pointer;
+			}
+		}
+	}
+	
 	return head;
 }
 
@@ -169,10 +205,24 @@ void plotVTU(int time_step, Node* head)
 
 	outFile << setiosflags(ios::fixed) << setiosflags(ios::showpoint) << setw(10) << setprecision(8);
 
+	int addOff = 0, i = 0, counter = 0, total = 0, *offset;
+	offset = new int[cells];
+	
+	for (i = 0; i < cells; i++)
+	{
+		if (i != 0)
+			offset[i] = nodes[i] + offset[i - 1];
+		
+		else
+			offset[i] = nodes[i];
+
+		total += nodes[i];
+	}
+
 	outFile << "<?xml version=\"1.0\"?>\n";
 	outFile << "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\" byte_order=\"LittleEndian\">\n";
 	outFile << "\t<UnstructuredGrid>\n";
-	outFile << "\t\t<Piece NumberOfPoints=\"" << nodes << "\" NumberOfCells=\"" << "1" << "\">\n";
+	outFile << "\t\t<Piece NumberOfPoints=\"" << total << "\" NumberOfCells=\"" << cells << "\">\n";
 
 	outFile << "\t\t\t<Points>\n";
 	outFile << "\t\t\t\t<DataArray type=\"Float32\" Name=\"Points\" NumberOfComponents=\"3\" format=\"ascii\">\n";
@@ -187,29 +237,56 @@ void plotVTU(int time_step, Node* head)
 	outFile << "\t\t\t</Points>\n";
 
 	outFile << "\t\t\t<Cells>\n";
-	outFile << "\t\t\t\t<DataArray type=\"Int64\" Name=\"connectivity\" format=\"ascii\">\n";
+	outFile << "\t\t\t\t<DataArray type=\"Int64\" Name=\"connectivity\" format=\"ascii\">";
 
 	l = head;
-	outFile << "\t\t\t\t\t" << l->value - 1;
-	while (l)
+	
+	for (i = 0; i < cells; i++)
 	{
-		if (l->next != NULL)
+		outFile <<"\n\t\t\t\t\t";
+		
+		if (i != 0)
+			addOff += nodes[i - 1];
+
+		outFile << l->value + addOff - 1;
+
+		for (counter = 1; counter < nodes[i]; counter++)
 		{
-			outFile << " ";
-			outFile << connectivity[l->value] - 1;
+			if (l && l->next)
+				outFile << " " << connectivity[l->value + addOff] - 1;
+
+			l = l->next;
 		}
-		l = l->next;
+
+		if (l)
+			l = l->next;
+	}
+	
+	outFile << "\n\t\t\t\t</DataArray>\n";
+
+	outFile << "\t\t\t\t<DataArray type=\"Int64\" Name=\"offsets\" format=\"ascii\">\n";
+	outFile << "\t\t\t\t\t";
+	
+	for (i = 0; i < cells; i++)
+	{
+		if (i != cells - 1)
+			outFile << offset[i] << " ";
+		else
+			outFile << offset[i];
 	}
 
 	outFile << "\n\t\t\t\t</DataArray>\n";
 
-	outFile << "\t\t\t\t<DataArray type=\"Int64\" Name=\"offsets\" format=\"ascii\">\n";
-	outFile << "\t\t\t\t\t" << nodes;
-
-	outFile << "\n\t\t\t\t</DataArray>\n";
-
 	outFile << "\t\t\t\t<DataArray type=\"UInt8\" Name=\"types\" format=\"ascii\">\n";
-	outFile << "\t\t\t\t\t4";
+	outFile << "\t\t\t\t\t";
+
+	for (i = 0; i < cells; i++)
+	{
+		if (i != cells - 1)
+			outFile << "4" << " ";
+		else
+			outFile << "4";	
+	}
 
 	outFile << "\n\t\t\t\t</DataArray>\n";
 
